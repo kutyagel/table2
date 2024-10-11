2abd5a4ed626af8548862277140634240673edf4
Racz Tibor Mate <racz.tibor@BOLYAI.LOCAL>
2024-10-11 14:37:44 +0200

    renderTable hozzaadas

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
+
+function renderTable(){
+    for (const pers of array) {
+        const tr_body = document.createElement("tr");
+        tbody.appendChild(tr_body);
+        const td_lastname = document.createElement("td");
+        td_lastname.innerHTML = pers.lastname;
+        tr_body.appendChild(td_lastname);
+        const td_firstname1 = document.createElement("td");
+        td_firstname1.innerHTML = pers.firstname1;
+        tr_body.appendChild(td_firstname1);
+      
+        if (pers.firstname2 === undefined) {
+            td_firstname1.colSpan = 2;
+        } else {
+            const td_firstname2 = document.createElement("td");
+            td_firstname2.innerHTML = pers.firstname2;
+            tr_body.appendChild(td_firstname2);
+        }
+        tr_body.addEventListener("click", function (e) {
+            console.log("clicked");
+            const a = tbody.querySelector(".selected")
+            if (a != undefined) {
+                a.classList.remove("selected")
+            }
+            e.currentTarget.classList.add("selected")
+        }
+
+        );
+        
+        const td_hazas = document.createElement("td")
+        td_hazas.innerHTML = pers.married;
+        tr_body.appendChild(td_hazas);
+        const td_pet = document.createElement("td")
+        td_pet.innerHTML = pers.pet;
+        tr_body.appendChild(td_pet);
+        
+        if(pers.married === true){
+        td_hazas.innerHTML="igen"
+            
+        }
+        else{
+         td_hazas.innerHTML="nem"
+        }
+    
+    }
+
+}
+
 let array = [
     {
         firstname1: 'Géza',
@@ -53,58 +102,39 @@ th_hazas.innerHTML = "Házas";
 th_pet.innerHTML = "Pet"
 th_firstname.colSpan = 2;
 
-for (let pers of array) {
-    const tr_body = document.createElement("tr");
-    tbody.appendChild(tr_body);
-    const td_lastname = document.createElement("td");
-    td_lastname.innerHTML = pers.lastname;
-    tr_body.appendChild(td_lastname);
-    const td_firstname1 = document.createElement("td");
-    td_firstname1.innerHTML = pers.firstname1;
-    tr_body.appendChild(td_firstname1);
-  
-    if (pers.firstname2 === undefined) {
-        td_firstname1.colSpan = 2;
-    } else {
-        const td_firstname2 = document.createElement("td");
-        td_firstname2.innerHTML = pers.firstname2;
-        tr_body.appendChild(td_firstname2);
-    }
-    tr_body.addEventListener("click", function (e) {
-        console.log("clicked");
-        const a = tbody.querySelector(".selected")
-        if (a != undefined) {
-            a.classList.remove("selected")
-        }
-        e.currentTarget.classList.add("selected")
-    });
-    
-    const td_hazas = document.createElement("td")
-    td_hazas.innerHTML = pers.married;
-    tr_body.appendChild(td_hazas);
-    const td_pet = document.createElement("td")
-    td_pet.innerHTML = pers.pet;
-    tr_body.appendChild(td_pet);
-    
-    if(pers.married === true){
-    td_hazas.innerHTML="igen"
-        
-    }
-    else{
-     td_hazas.innerHTML="nem"
-    }
 
-}
 const form =document.getElementById("form");
 form.addEventListener("submit",function(e){
+    e.preventDefault()
     const lastname = document.getElementById("lastname");
     const firstname1 = document.getElementById("firstname1");
     const firstname2 = document.getElementById("firstname2");
-    const married = document.getElementById("checkbox");
+    const married = document.getElementById("married");
     const pet = document.getElementById("pet");
     const lastnamevalue=lastname.value;
     const firstname1value=firstname1.value;
-    const firstname2value=firstname2.value;
+    let firstname2value=firstname2.value;
     const marriedvalue=married.checked;
     const petvalue=pet.value; 
-});
+
+    if (firstname2value === '') {
+        firstname2value=undefined;
+    }
+    const newPerson =
+        {
+            firstname1: firstname1value,
+            firstname2: firstname2value,
+            lastname: lastnamevalue,
+            married: marriedvalue,
+            pet: petvalue
+        }
+    array.push(newPerson)
+    console.log(array)
+    tbody.innerHTML = ""
+    renderTable()
+
+
+}
+);
+renderTable()
+
